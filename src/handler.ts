d3d50788d28ebec8219d6d492eed69d9bc1ae581
Juan Morales <juank.memphis@gmail.com>
2021-06-06 12:07:30 -0600

    adds a centralized way to log errors (#117)

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,24 @@
 import { ApolloServer } from 'apollo-server-lambda'
 import { Context, APIGatewayProxyEvent, APIGatewayProxyCallback } from 'aws-lambda'
+import { GraphQLError } from 'graphql'
+import { get } from 'lodash'
 
 import { schema, context, plugins } from './graphql'
-import { createNeo4JDriver } from './shared'
+import { createNeo4JDriver, errorsLogger } from './shared'
 
 const apolloServer = new ApolloServer({
   schema,
   context,
   plugins,
+  formatError: (err: GraphQLError) => {
+    const stackTrace = get(err, 'extensions.exception.stacktrace')
+    errorsLogger.error(
+      'Unexpected error occurred on a graphql resolver...',
+      err.message,
+      stackTrace,
+    )
+    return err
+  },
 })
 
 const graphqlHandler = apolloServer.createHandler({
